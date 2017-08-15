acdc90d7d59c09ef0769f5c2df3e71ad9a901982
jfernace <Fernjl02@gmail.com>
2017-08-15 14:50:15 -0400

    Add Spice Refinery to Dune Planet

--- kitten-scientistsvJF2.users.js
+++ kitten-scientistsvJF2.users.js
@@ -510,7 +510,8 @@ var run = function() {
                     // Dune
                     planetCracker:  {require: 'science',     enabled: false},
                     hydrofracturer: {require: 'science',     enabled: false},
-
+		    spiceRefinery:  {require: 'science',     enabled: false},
+			
                     // Piscine
                     researchVessel: {require: 'titanium',    enabled: false},
                     orbitalArray:   {require: 'eludium',     enabled: false},
